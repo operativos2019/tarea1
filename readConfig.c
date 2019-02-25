f85dde8fa17bda3ecc861fba30ba793a4e92113d
JeanPaul <jeanpaullm@gmail.com>
2019-02-25 16:08:07 -0600

    finished read config script

--- readConfig.c
+++ readConfig.c
@@ -3,56 +3,62 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 #define CONFIG_FILE_DIR   "/etc/webserver"
 #define CONFIG_FILE_PATH  "/etc/webserver/config.conf"
-#define CONFIG_FILE_DATA  "PORT=8081"
+#define CONFIG_FILE_DEFAULT_PORT "PORT=8081"
+#define CONFIG_FILE_DEFAULT_LOG_PATH "LOGFILE=/lo/que/gabito/quiera"  
 
 struct stat s; 
 
 /*
- * En caso de que el directorio especificado no exista lo crea
+ * En caso de que el directorio especificado exista
+ * de lo contrario lo crea
  */
-void verifyDir() {
+void verifyConfigDir() {
 
    int err = stat(CONFIG_FILE_DIR, &s);
 
    if(-1 == err) {
       mkdir(CONFIG_FILE_DIR, 0700);
-      printf("Directorio creado.\n");
+      printf("Directorio de configuracion creado.\n");
    }
 
 }
 
 /*
- * Funcion que crea el archivo de configuracion en caso que exista
+ * Funcion que verifica que el archivo de configuracion exista
+ * en caso contrario lo crea
  */
 void createConfigFile() {
 
-   verifyDir();
+   verifyConfigDir();
    
    FILE* file = fopen(CONFIG_FILE_PATH, "w");
    
    if(file == NULL) {
-      printf("No fue posible crear el archivo de configuración.\n Intente con sudo ./ejecutable");
+      printf("No fue posible crear el archivo de configuración.\n Intente con sudo ./ejecutable\n");
       exit(EXIT_FAILURE);
    }
    
    else {
-      fputs(CONFIG_FILE_DATA, file);
+      fputs(CONFIG_FILE_DEFAULT_PORT , file);
+      fputs("\n\n" , file);
+      fputs(CONFIG_FILE_DEFAULT_LOG_PATH, file);
       fclose(file);
    }
 
-   printf("Archivo creado.\n");
+   printf("Archivo de configuración creado.\n");
       
 }
 
 /*
  *  Funcion que retorna el puerto especificado en el archivo de configuracion
+ *  si no lo encuentra retorna NULL
  */
-int readConfigFile() {
+int* getPortFromConfigFile() {
 
-   int port;
    FILE* file;
  
    file = fopen(CONFIG_FILE_PATH, "r"); // read mode
@@ -67,12 +73,18 @@ int readConfigFile() {
       exit(EXIT_FAILURE);
    }
 
-   if(fscanf(file, "PORT=%d", &port) == 1){ 
-      if (port < 1024 || port > 65536|| port == 0 ) { 
-         printf("ell puerto: %d, no es valido\n", port); 
-         exit(1); 
-      } 
-      else printf("el puerto es: %d\n", port);
+   int* port = calloc(1, sizeof(int));
+
+   while(!feof(file)) {
+      if(fscanf(file,"PORT=%d", port) == 1) {
+         break;
+      }
+      fgetc(file);
+   }
+
+   if((port != NULL) && (*port == '\0')) {
+      printf("No se encontró el puerto en el archivo de configuración\n"); 
+      return NULL;
    }
 
    fclose(file);
@@ -81,10 +93,53 @@ int readConfigFile() {
 
 }
 
+/*
+ *  Funcion que retorna la ruta al log file del webserver
+ *  si no lo encuentra retorna NULL
+ */
+char* getLogPathFromConfigFile() {
+   
+   FILE* file;
+
+   file = fopen(CONFIG_FILE_PATH, "r");
+
+   if(file == NULL) {
+      printf("No fue posible leer el archivo de configuración\n");
+      exit(EXIT_FAILURE);
+   }
+
+   char* logFilePath = (char*)calloc(256, sizeof(char));
+
+   while(!feof(file)) {
+      if(fscanf(file,"LOGFILE=%s", logFilePath) == 1) {
+         break;
+      }
+      fgetc(file);
+   }
+
+   if((logFilePath != NULL) && (logFilePath[0] == '\0')) {
+      printf("No se encontró el logfile path en el archivo de configuración\n"); 
+      return NULL;
+   }
+
+   fclose(file);
+
+   return logFilePath;
+
+}
 
 int main() {
 
-   readConfigFile();
+   int* port = getPortFromConfigFile();
+   char* logFilePath = getLogPathFromConfigFile();
+
+   if(port != NULL) {
+      printf("Puerto = %d\n",*port);   
+   }
+
+   if(logFilePath != NULL) {
+      printf("LogFile path = %s\n",logFilePath);   
+   }
 
    return 0;
 
